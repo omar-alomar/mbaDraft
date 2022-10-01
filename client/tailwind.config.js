f6c0efd528b9aad177e9eeb34a55c098d25d67a9
ZedRmzi <zaid-ramzi@hotmail.com>
2022-10-01 19:30:25 -0400

    theme and index.css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,10 +10,17 @@ module.exports = {
       },
       colors: {
         mbaRed: '#6B293C',
-        hoverRed: 'B85873',
-        grey: 'F5F5F5'
+        hoverRed: '#B85873',
+        'submit': {
+          DEFAULT: '#34CF7F',
+          'hover': '#289E61'
+        },
+        'edit': {
+          DEFAULT: '#E4B538',
+          'hover': '#E8C76D'
+        }
       }
-    }
+    },
   },
   plugins: [],
 }
