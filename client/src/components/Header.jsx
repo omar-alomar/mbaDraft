262725a136e8f21e93848913679b8f58a5014547
Omar Alomar <omaro2000@gmail.com>
2022-11-13 21:12:18 -0500

    header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 // this is a comment
 const Header = ({Text}) => {
   return (
-<<<<<<< HEAD
     <div className='w-full'>
       <div className='flex w-full bg-[#DBDADA] font-Fira'>
         <div className='basis-1/2'>
@@ -12,14 +11,6 @@ const Header = ({Text}) => {
         <div className='basis-1/2 text-right my-auto text-mbaRed'>
           <h1 className='text-[2.3rem] pr-10'>{Text}</h1>
         </div>
-=======
-    <div className='flex w-full bg-[#CBCBCB] font-Fira border-solid border-b-[1px] border-mbaRed'>
-      <div className='basis-1/2'>
-        <img className='mr-auto pl-10 py-5' src='\Logo(edited).png'></img>
-      </div>
-      <div className='basis-1/2 text-right my-auto text-mbaRed'>
-        <h1 className='text-[2.3rem] pr-10'>{Text}</h1>
->>>>>>> dc93787a17f9b80a4ed28875f314e3f8371484d7
       </div>
     </div>
   )
