a8dc5439837196f557cffa7805776f120a7d8f10
ZedRmzi <zaid-ramzi@hotmail.com>
2022-11-13 20:29:50 -0500

    slight header changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,12 +4,11 @@ import React from 'react'
 const Header = ({Text}) => {
   return (
     <div className='flex w-full bg-[#DBDADA] font-Fira'>
-      <div className='basis-2/12'>
-        <img className='mx-auto py-5' src='\Logo(edited).png'></img>
+      <div className='basis-1/2'>
+        <img className='mr-auto pl-10 py-5' src='\Logo(edited).png'></img>
       </div>
-      <div className='basis-8/12 my-auto'></div>
-      <div className='basis-2/12 my-auto mx-auto text-mbaRed'>
-        <h1 className='text-[2rem]'>{Text}</h1>
+      <div className='basis-1/2 text-right my-auto text-mbaRed'>
+        <h1 className='text-[2.3rem] pr-10'>{Text}</h1>
       </div>
     </div>
   )
